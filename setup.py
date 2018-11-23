3de97c18252a4d1116ea984a0135c8a33efbd010
k0t3n <k0t3n.mail@gmail.com>
2018-11-23 21:10:27 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='django-admitad',
+    version='0.0.1',
+    packages=['admitad'],
+    install_requires=[
+        'Django>=2.0',
+    ],
+    url='https://github.com/k0t3n/django-admitad',
+    license='MIT License',
+    author='k0t3n',
+    author_email='k0t3n.mail@gmail.com',
+    description='Django application that integrates Admitad CPA using postback requests',
+    classifiers=[
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+)
