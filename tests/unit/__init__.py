3ec82c49eb29c51a51fd791f1ca121082155a2a4
k0t3n <k0t3n.mail@gmail.com>
2018-11-26 14:05:23 +0300

    Add system part insert to unit __init__.py

--- tests/unit/__init__.py
+++ tests/unit/__init__.py
@@ -1,6 +1,11 @@
 import os
+import sys
 
 import django
 
-os.environ["DJANGO_SETTINGS_MODULE"] = 'tests.integration.settings'
+abspath = lambda *p: os.path.abspath(os.path.join(*p))
+PROJECT_ROOT = abspath(os.path.dirname(__file__))
+ADMITAD_TESTS_MODULE_PATH = abspath(PROJECT_ROOT, '..')
+sys.path.insert(0, ADMITAD_TESTS_MODULE_PATH)
+os.environ["DJANGO_SETTINGS_MODULE"] = 'integration.settings'
 django.setup()
