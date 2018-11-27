694bab4840505d586a582f9d20b718556cd6b6e8
Alexey Kotenko <k0t3n.mail@gmail.com>
2018-11-27 14:48:42 +0300

    (New) Add django checks for settings and some minor fixes (#6)
    * Replace postback_key to settings
    
    * Fix order tests
    
    * Replace tariff_code from Order to Item object
    
    * Fix tests
    
    * Add django checks for admitad settings
    
    Add REQUIRED_SETTINGS, DOCS_URL and missing default_app_config into
    admitad __init__.py
    
    Add settings_check() method for checking missed settings
    
    Add manage.py to tests
    
    * Fix start from number in enumerated cycle in _generate_postback_payload()
    method

--- admitad/apps.py
+++ admitad/apps.py
@@ -1,7 +1,29 @@
 from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
+from django.core.checks import Error, register
+from django.conf import settings
+
+from admitad import REQUIRED_SETTINGS, DOCS_URL
+
 
 class AdmitadConfig(AppConfig):
     name = 'admitad'
     verbose_name = _('django admitad')
+
+
+@register
+def settings_check(app_configs, **kwargs):
+    errors = []
+
+    for number, required_field in enumerate(REQUIRED_SETTINGS, start=1):
+        if not hasattr(settings, required_field):
+            errors.append(
+                Error(
+                    msg='{} not found'.format(required_field),
+                    hint='Please, read installation guide ({})'.format(DOCS_URL),
+                    obj='django-admitad',
+                    id='admitad.S{}'.format(number),
+                )
+            )
+    return errors
