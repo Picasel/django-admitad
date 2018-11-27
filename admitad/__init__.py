99ba1f7ddfbd3bdd403f3a02e24bc8ff8182863c
k0t3n <k0t3n.mail@gmail.com>
2018-11-27 14:29:52 +0300

    Add django checks for admitad settings
    Add REQUIRED_SETTINGS, DOCS_URL and missing default_app_config into
    admitad __init__.py
    
    Add settings_check() method for checking missed settings
    
    Add manage.py to tests

---
+++ admitad/__init__.py
@@ -0,0 +1,4 @@
+default_app_config = 'admitad.apps.AdmitadConfig'
+
+REQUIRED_SETTINGS = ['ADMITAD_COMPAIN_CODE', 'ADMITAD_POSTBACK_URL', 'ADMITAD_POSTBACK_KEY']
+DOCS_URL = 'https://github.com/Picasel/django-admitad'
