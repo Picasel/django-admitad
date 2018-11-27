1ed9d88705b55c80069a54adda2c1511d0d948fb
k0t3n <k0t3n.mail@gmail.com>
2018-11-27 14:52:16 +0300

    Release 1.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-admitad',
-    version='1.0.2',
+    version='1.0.3',
     packages=['admitad'],
     install_requires=[
         'Django>=2.0',
