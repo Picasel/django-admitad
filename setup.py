47cd1c81ba4d839f0fbd60b4278d0cd47219162e
k0t3n <k0t3n.mail@gmail.com>
2018-12-21 13:48:11 +0300

    Update Python versions in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-admitad',
-    version='1.1.0',
+    version='1.1.1',
     packages=['admitad'],
     package_data={
         'admitad': ['migrations/*.py']
@@ -20,6 +20,10 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Topic :: Software Development :: Libraries :: Python Modules"
     ],
