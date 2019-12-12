4f03dd7442161bea0c826d4499fbd928b44db52d
Igor <ozmundsedler@mail.ru>
2019-12-12 13:22:23 +0300

    new v

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-admitad',
-    version='1.1.1',
+    version='1.1.2',
     packages=['admitad'],
     package_data={
         'admitad': ['migrations/*.py']
